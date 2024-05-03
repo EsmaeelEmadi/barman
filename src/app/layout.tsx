95cfd70c47b905d98162cc1d198679efec03a19f
emad <emadi68moh@gmail.com>
2024-05-03 20:16:00 +0000

    Adding material theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { theme } from "@/themes/material/mainTheme";
+import { ThemeProvider } from "@mui/material/styles";
+
+// STYLES
 import "./globals.css";
+import { CssBaseline } from "@mui/material";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +23,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
