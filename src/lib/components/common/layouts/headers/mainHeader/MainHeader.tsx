05dc2a07ef579e43400a77092f890206f20dd2c2
emad <emadi68moh@gmail.com>
2024-05-04 00:36:05 +0000

    init MainHeader

---
+++ src/lib/components/common/layouts/headers/mainHeader/MainHeader.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// TYPES
+import type { FC } from "react";
+
+const MainHeader: FC = () => {
+  return <div data-testid="main-header">header test</div>;
+};
+
+export default MainHeader;
