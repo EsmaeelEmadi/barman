2cd7f4b20f2cc82c71e73a70d6156283a4e61d75
emad <emadi68moh@gmail.com>
2024-05-09 10:15:09 +0000

    init

--- src/lib/components/common/layouts/headers/mainHeader/MainHeader.tsx
+++ src/lib/components/common/layouts/headers/mainHeader/MainHeader.tsx
@@ -1,10 +1,60 @@
-import React from "react";
+// COMPONENTS
+import { AppBar, Button, Container, Toolbar, Box } from "@/material/index";
+
+// COMPONENTS
+import Image from "next/image";
+import Link from "next/link";
+
+import UserTooltopClient from "./UserTooltipClient";
+import MenuTooltipClient from "./MenuTooltipClient"
+
+
+// CONSTANS
+import {PAGES} from "./constants";
 
 // TYPES
 import type { FC } from "react";
 
-const MainHeader: FC = () => {
-  return <div data-testid="main-header">header test</div>;
-};
 
+const MainHeader: FC = () => {
+  return (
+    <AppBar position="static" color="transparent" elevation={3}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box className="flex flex-1 gap-4">
+            <Link href='/' className="flex justify-center">
+            <Image
+              src="/images/svgs/logo.svg"
+              alt="website logo"
+              priority
+              width={150}
+              height={100}
+              className="mt-2"
+            />
+            </Link>
+
+            <Box
+              sx={{ flexGrow: 0, display: { xs: "none", md: "flex" } }}
+            >
+              {PAGES.map((page) => (
+                <Button
+                  key={page}
+                  sx={{ my: 2, display: "block" }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+          </Box>
+
+          <Box className="flex items-center">
+            <UserTooltopClient />
+            <MenuTooltipClient />
+          </Box>
+
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
 export default MainHeader;
