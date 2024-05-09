9d4e99349226a204b0c301e1413e2b9711f1a0c0
emad <emadi68moh@gmail.com>
2024-05-09 10:14:44 +0000

    added customized default shadow

--- src/lib/themes/material/mainTheme.tsx
+++ src/lib/themes/material/mainTheme.tsx
@@ -3,6 +3,20 @@
 import { createTheme, type ThemeOptions } from "@mui/material/styles";
 
 const options: ThemeOptions = {
+  // @ts-ignore didn't wanted to add all the 32 level :)
+  shadows: [
+    'none', // elevation 0
+    '0 2px 4px rgba(0, 0, 0, 0.1)', // elevation 1
+    '0 4px 8px rgba(0, 0, 0, 0.1)', // elevation 2
+    '0 8px 16px rgba(0, 0, 0, 0.1)', // elevation 3
+    '0 16px 24px rgba(0, 0, 0, 0.1)', // elevation 4
+    '0 24px 32px rgba(0, 0, 0, 0.1)', // elevation 5
+    '0 28px 36px rgba(0, 0, 0, 0.1)', // elevation 6
+    '0 32px 36px rgba(0, 0, 0, 0.1)', // elevation 7
+    '0 36px 40px rgba(0, 0, 0, 0.1)', // elevation 8
+    // add more
+    // ...
+  ],
   palette: {},
 };
 
